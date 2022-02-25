9d730ebd18784964fcba0775d4f835f3debc2cee
EzeTP <tejada.eap@gmail.com>
2022-02-25 13:39:24 -0300

    adding new loading screens

--- client/src/components/loading/Loading.jsx
+++ client/src/components/loading/Loading.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./loading.scss";
 import img from "../../assets/ZkIX.gif";
-import img2 from "../../assets/33HU.gif";
-import img3 from "../../assets/hound-dog.gif";
-import img4 from "../../assets/X9Yq.gif";
+import img2 from "../../assets/st1.gif";
+import img3 from "../../assets/st2.gif";
+import img4 from "../../assets/st3.gif";
+import img5 from "../../assets/X9Yq.gif";
+import img6 from "../../assets/X9Yq.gif";
+import img7 from "../../assets/X9Yq.gif";
+import img8 from "../../assets/X9Yq.gif";
+import img9 from "../../assets/X9Yq.gif";
+import img10 from "../../assets/X9Yq.gif";
+import img11 from "../../assets/X9Yq.gif";
 
 const Loading = () => {
   let arr = [img, img2, img3, img4];
