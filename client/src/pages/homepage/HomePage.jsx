37ef00514f8d479c380a52168fa577dbf50c6b7b
EzeTP <tejada.eap@gmail.com>
2022-02-24 23:52:39 -0300

    adding some tets, fixing some css

--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -4,12 +4,21 @@ import GameCards from "../../components/gamecards/GameCards";
 import SearchBar from "../../components/searchbar/SearchBar";
 import Order from "../../components/order/Order";
 import Filter from "../../components/filter/Filter";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const handleOnClick = () => {
+    navigate("/create");
+  };
+
   return (
     <div className="home">
       <div className="span">
         <span className="span1">EzeTP </span>
+        <span className="span2" onClick={handleOnClick}>
+          Create A Game!{" "}
+        </span>
       </div>
       <div className="searchContainer">
         <Filter />
